574f9a906577d600401fd2850f91f9c252287249
johadi <jim.hadi10@gmail.com>
2017-07-03 13:49:13 +0100

    adding the getPrime number function into its file

--- src/primes.js
+++ src/primes.js
@@ -1,1 +1,28 @@
+//Project for generating prime numbers
+//find out if a single number is a prime number
+let isPrime=(number)=>{
+    if(number <= 1){
+        return false;
+    }
+    for(let i=2;i < number; i++){
+        if(number % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+//generate prime numbers from 0 to the numbers input
+let getPrimes=(number)=>{
+    let prime_numbers=[];
+    if(typeof number !='number'){
+        return 'undefined';
+    }
+    for(let i=0; i <= number; i++){
+        if(isPrime(i)){
+            prime_numbers.push(i);
+        }
+    }
+    return prime_numbers;
+}
 
+module.exports={getPrimes}
